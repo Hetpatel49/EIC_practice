eb3d43a0950785cae4acf616266c1bdbbf354309
Hetpatel49 <patelhet656@gmail.com>
2024-02-13 01:44:19 +0530

    application code level-2

--- movieticket/application.c
+++ movieticket/application.c
@@ -1,29 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include "header.h"
 
 int main()
 {
-    tickets = (ticket_t *)calloc(100, sizeof(ticket_t));
-    int **seat, choice, price = 500, selected_seat, i;
-    seat = (int **)calloc(2, sizeof(int *));
-    for (i = 0; i < 2; i++)
-        *(seat + i) = (int *)calloc(50, sizeof(int));
-    int x;
-    ticket_t *t = (ticket_t *)calloc(1, sizeof(ticket_t));
-    init(t);
+    tickets = calloc(300, sizeof *(tickets));
+    int **seat;          // total 6 screens adn 50 seats in each screen
+    int16_t choice;      // to select the option
+    int16_t price = 500; // price of each ticket with default value
+    int i;
+    seat = calloc(6, sizeof(*seat)); // allocating memory for 6 screens
+    for (i = 0; i < 6; i++)
+        seat[i] = calloc(50, sizeof *(seat[i]));
+    int x; // to exit the loop
+
+    history = (char **)calloc(10, sizeof(char *)); // allocating memory for history records
+    for (i = 0; i < 10; i++)
+        *(history + i) = (char *)calloc(100, sizeof(char)); // allocating memory for each history string record
 
     while (x != 5)
     {
         choice = selectoption();
+
         switch (choice)
         {
         case 1:
         {
-            int selected_seat;
-            selected_seat = selectmovie(t);
-            reservation(seat[selected_seat - 1], t);
+            int total_tickets = 0;
+            printf("Enter total number of tickets to buy:");
+            scanf("%d", &total_tickets);
+            while (total_tickets--)
+            {
+                ticket_t ticket;
+                init(&ticket);
+                int selected_screentime;
+                selected_screentime = selectmovie(&ticket);
+                reservation(seat[selected_screentime], &ticket);
+                tickets[ticket_pointer++] = ticket;
+            }
             break;
         }
 
@@ -34,17 +50,33 @@ int main()
         }
 
         case 3:
+        {
+            for (int i = 0; i < history_pointer; i++)
+                printf("%s\n", *(history + i));
             break;
+        }
 
         case 4:
         {
-            printf("Screen 1:Lagan special\nScreen 2: Hurry Om Hurry\nEnter Screen No.: ");
-            int screen;
+            printf("Screen 1: Lagan special\nScreen 2: Hurry Om Hurry\nEnter Screen No.: ");
+            int screen, time;
             scanf("%d", &screen);
-            if (screen == 1)
+            printf("Available timings:\n1. 10:00 to 13:00\n2. 14:00 to 17:00\n3. 19:00 to 22:00\n");
+            printf("Choose the time:");
+            scanf("%d", &time);
+            if (screen == 1 && time == 1)
                 print_available_seats(seat[0]);
-            else if (screen == 2)
+            else if (screen == 1 && time == 2)
                 print_available_seats(seat[1]);
+            else if (screen == 1 && time == 3)
+                print_available_seats(seat[2]);
+            else if (screen == 2 && time == 1)
+                print_available_seats(seat[3]);
+            else if (screen == 2 && time == 2)
+                print_available_seats(seat[4]);
+            else if (screen == 2 && time == 3)
+                print_available_seats(seat[5]);
+
             else
                 printf("Invalid choice\n");
             break;
@@ -62,5 +94,6 @@ int main()
             break;
         }
         }
+	
     }
 }
